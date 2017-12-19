2fa8894fe03157a4faa381d7a0ca33cf79abfec9
Cause Chung <cuzfrog@139.com>
2017-12-19 13:06:11 +0800

    Add nested object to JsObjectConvertibleTest

--- tests/src/test/scala/sri/global/JsObjectConvertibleTest.scala
+++ tests/src/test/scala/sri/global/JsObjectConvertibleTest.scala
@@ -10,13 +10,20 @@ import scala.scalajs.js
 import scala.util.Random
 
 object JsObjectConvertibleTest extends BaseSuite {
+
+
   private def genTestClass() =
-    TestClass(Random.genAlphanumeric(10), Random.nextInt(), () => println("callback!"))
+    TestClass(
+      Random.genAlphanumeric(10),
+      Random.nextInt(),
+      () => println("callback!"),
+      Option(new js.Object())
+    )
 
   test("to js object") {
     val testClass = this.genTestClass()
     val json = JSON.stringify(testClass.toJsObject)
-    val expectedJson = s"""{"f1":"${testClass.f1}","v1":${testClass.v1}}"""
+    val expectedJson = s"""{"f1":"${testClass.f1}","v1":${testClass.v1},"nested":{"value$$2":{}}}"""
     expect(json).toBe(expectedJson)
     console.log(JSON.stringify(testClass.toJsObject))
   }
@@ -26,7 +33,8 @@ object JsObjectConvertibleTest extends BaseSuite {
     val obj = js.Dynamic.literal(
       f1 = testClass.f1,
       v1 = testClass.v1,
-      callback = testClass.callback
+      callback = testClass.callback,
+      nested = testClass.nested.asInstanceOf[js.Any]
     )
     val parsed = obj.toScalaClass[TestClass]
     expect(parsed).toEqual(testClass)
@@ -47,5 +55,8 @@ object JsObjectConvertibleTest extends BaseSuite {
     expect(testClass.toJsObject.toScalaClass[TestClass]).toEqual(testClass)
   }
 
-  private case class TestClass(f1: String, v1: Int, callback: js.Function0[Unit])
+  private case class TestClass(f1: String,
+                               v1: Int,
+                               callback: js.Function0[Unit],
+                               nested: Option[AnyRef])
 }
